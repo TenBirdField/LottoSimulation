7e20d265281f3df953e67f0a75981ea5e757b911
yonghlee <cjswl1357@gmail.com>
2024-12-25 20:36:19 +0900

    version 0.1

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/lotto-simulator/' // repository 이름을 base로 설정
+  base: '/LottoSimulation/' // repository 이름을 base로 설정
 })
